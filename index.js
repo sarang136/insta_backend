b62a47dccaa428536d9ca2639ed25c14a725f992
gopi-gh <gopi-gh@d3vtech.com>
2025-09-13 15:25:00 +0530

    again

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors({
-    origin:["http://localhost:5173","http://localhost:5174"],
+    origin:["http://localhost:5173","http://localhost:5174", "https://insta-frontend-seven.vercel.app"],
     credentials: true, methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
 }))
